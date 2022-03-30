49933d9706ce5903a49587fa4732ecd2a4f85854
John Harry Harlow <john.harlow@bench.co>
2022-03-30 16:07:25 -0700

    CFunction to createIncomeStatement object

--- src/hooks/useIncomeStatement.js
+++ src/hooks/useIncomeStatement.js
@@ -1,16 +1,20 @@
 import { useState } from 'react';
 import useTransactionStore from './useTransactionStore';
-import categories from '../_mocks_/categories';
+import accountTypes from '../_mocks_/categories';
 
 function useIncomeStatement(startDate, endDate) {
   const [transactions] = useTransactionStore();
+
 
   const incomestatement = createIncomeStatement(transactions, startDate, endDate);
 
   return incomestatement;
 }
 
+
+
 export const createIncomeStatement = (transactions, startDate, endDate) => {
+<<<<<<< Updated upstream
   const incomestatement = transactions.reduce(
     (acc) => {
       return acc;
@@ -22,8 +26,41 @@ export const createIncomeStatement = (transactions, startDate, endDate) => {
       Profit: { gross: 0, net: 0 }
     }
   );
+=======
+  const initialValue = 0;
+  /*const incomestatement = transactions.reduce((sum, p) => sum + p.revenuetotal*p.total, 0, {
+    revenues: { total: 0 },
+    costOfSales: { total: 0 },
+    expenses: { total: 0 },
+    profit: { gross: 0, net: 0 }
+  });*/
+>>>>>>> Stashed changes
 
-  return incomestatement;
+  const incomeStatement = {
+    revenues: { total: 1000 },
+    costOfSales: { total: 0 },
+    expenses: { total: 0 },
+    profit: { gross: 0, net: 0 }
+  };
+
+  const filteredTxns = transactions.filter(txn => txn.date < startDate && txn.date > endDate);
+
+  //Put in filteredTxns arr
+  const setServiceRevenue = arr => {
+    let serviceRevenueSum = 0;
+    arr.forEach(
+      function(item) {
+        if (item.category === 'Service Revenue') {
+          serviceRevenueSum += item.total;
+        }
+      }
+    )
+    incomeStatement.revenues.serviceRevenue = serviceRevenueSum;
+  }
+
+  setServiceRevenue(transactions);
+
+  return incomeStatement;
 };
 
 export default useIncomeStatement;
